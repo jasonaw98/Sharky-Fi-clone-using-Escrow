5d2c559cb1451b173e16bafb5896f2ee64e407e4
Jason <jason.aw@attspace.ventures>
2023-11-07 15:46:57 +0800

    commit

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("EBDbmDQEoTCRUPg39R36iEHMNJw6R2tgchcprJBD486K");
 
 #[program]
 pub mod nft_lend_borrow {
